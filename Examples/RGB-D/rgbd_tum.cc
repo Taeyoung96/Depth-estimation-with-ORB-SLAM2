309890feb78cc40d641ab5a0c364762a8be49b0c
=Taeyoung Kim <tyoung96@naver.com>
2020-11-20 01:59:15 +0900

    first commit

---
+++ Examples/RGB-D/rgbd_tum.cc
@@ -0,0 +1,250 @@
+/**
+* This file is part of ORB-SLAM2.
+*
+* Copyright (C) 2014-2016 Raúl Mur-Artal <raulmur at unizar dot es> (University of Zaragoza)
+* For more information see <https://github.com/raulmur/ORB_SLAM2>
+*
+* ORB-SLAM2 is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM2 is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with ORB-SLAM2. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+#include<opencv2/core/core.hpp>
+#include<opencv2/opencv.hpp>
+#include<System.h>
+
+#include<librealsense2/rs.hpp>  // Include RealSense Cross Platform API
+
+#define UsingWebCam
+
+using namespace std;
+using namespace cv;
+
+void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
+                vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps);
+
+int main(int argc, char **argv)
+{
+#ifdef UsingWebCam
+    if(argc != 3)
+    {
+        cerr << endl << "argc:" << argc << "!= 3"<< endl;
+        return -1;
+    }
+
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+
+    rs2::config cfg;
+    cfg.enable_stream(RS2_STREAM_DEPTH);
+    cfg.enable_stream(RS2_STREAM_COLOR);
+
+    // Start streaming with default recommended configuration
+    pipe.start();
+
+    // Define two align objects. One will be used to align
+    // to depth viewport and the other to color.
+    // Creating align object is an expensive operation
+    // that should not be performed in the main loop
+    rs2::align align_to_color(RS2_STREAM_COLOR);
+
+    //const auto window_name = "Display Image";
+    //namedWindow(window_name,CV_WINDOW_AUTOSIZE);
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+
+#ifdef COMPILEDWITHC11
+    std::chrono::steady_clock::time_point initT = std::chrono::steady_clock::now();
+#else
+    std::chrono::monotonic_clock::time_point initT = std::chrono::monotonic_clock::now();
+#endif
+
+    while(true)
+    {
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+        auto aligned_frames = align_to_color.process(data);
+
+        rs2::depth_frame depth = aligned_frames.get_depth_frame();
+        rs2::frame color = aligned_frames.get_color_frame();
+
+        const int w_depth = depth.get_width();
+        const int h_depth = depth.get_height();
+
+        const int w_img = color.as<rs2::video_frame>().get_width();
+        const int h_img = color.as<rs2::video_frame>().get_height();
+
+        // Create OpenCV matrix of size (w,h) from the colorized depth data
+        Mat imageDepth(Size(w_depth, h_depth), CV_16UC1, (void*)depth.get_data(), Mat::AUTO_STEP);
+        // Create OpenCV matrix of size (w,h) from the RGB image data
+        Mat imageRGB(Size(w_img, h_img), CV_8UC3, (void*)color.get_data(), Mat::AUTO_STEP);
+        cv::cvtColor(imageRGB,imageRGB,CV_BGR2RGB); // Convert BGR to RGB
+
+        // Resize imageRGB to make same width and height with image
+        cv::resize(imageDepth,imageDepth,Size(512,256));
+        cv::resize(imageRGB,imageRGB,Size(512,256));
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point nowT = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point nowT = std::chrono::monotonic_clock::now();
+#endif
+        // Pass the image to the SLAM system
+        SLAM.TrackRGBD(imageRGB,imageDepth,std::chrono::duration_cast<std::chrono::duration<double> >(nowT-initT).count());
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    SLAM.SaveTrajectoryTUM("CameraTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+#else
+    if(argc != 5)
+    {
+        cerr << endl << "Usage: ./rgbd_tum path_to_vocabulary path_to_settings path_to_sequence path_to_association" << endl;
+        return 1;
+    }
+
+    // Retrieve paths to images
+    vector<string> vstrImageFilenamesRGB;
+    vector<string> vstrImageFilenamesD;
+    vector<double> vTimestamps;
+    string strAssociationFilename = string(argv[4]);
+    LoadImages(strAssociationFilename, vstrImageFilenamesRGB, vstrImageFilenamesD, vTimestamps);
+
+    // Check consistency in the number of images and depthmaps
+    int nImages = vstrImageFilenamesRGB.size();
+    if(vstrImageFilenamesRGB.empty())
+    {
+        cerr << endl << "No images found in provided path." << endl;
+        return 1;
+    }
+    else if(vstrImageFilenamesD.size()!=vstrImageFilenamesRGB.size())
+    {
+        cerr << endl << "Different number of images for rgb and depth." << endl;
+        return 1;
+    }
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::RGBD,true);
+
+    // Vector for tracking time statistics
+    vector<float> vTimesTrack;
+    vTimesTrack.resize(nImages);
+
+    cout << endl << "-------" << endl;
+    cout << "Start processing sequence ..." << endl;
+    cout << "Images in the sequence: " << nImages << endl << endl;
+
+    // Main loop
+    cv::Mat imRGB, imD;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        // Read image and depthmap from file
+        imRGB = cv::imread(string(argv[3])+"/"+vstrImageFilenamesRGB[ni],CV_LOAD_IMAGE_UNCHANGED);
+        imD = cv::imread(string(argv[3])+"/"+vstrImageFilenamesD[ni],CV_LOAD_IMAGE_UNCHANGED);
+        double tframe = vTimestamps[ni];
+
+        if(imRGB.empty())
+        {
+            cerr << endl << "Failed to load image at: "
+                 << string(argv[3]) << "/" << vstrImageFilenamesRGB[ni] << endl;
+            return 1;
+        }
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+#endif
+
+        // Pass the image to the SLAM system
+        SLAM.TrackRGBD(imRGB,imD,tframe);
+
+#ifdef COMPILEDWITHC11
+        std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+#else
+        std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+#endif
+
+        double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+
+        vTimesTrack[ni]=ttrack;
+
+        // Wait to load the next frame
+        double T=0;
+        if(ni<nImages-1)
+            T = vTimestamps[ni+1]-tframe;
+        else if(ni>0)
+            T = tframe-vTimestamps[ni-1];
+
+        if(ttrack<T)
+            usleep((T-ttrack)*1e6);
+    }
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Tracking time statistics
+    sort(vTimesTrack.begin(),vTimesTrack.end());
+    float totaltime = 0;
+    for(int ni=0; ni<nImages; ni++)
+    {
+        totaltime+=vTimesTrack[ni];
+    }
+    cout << "-------" << endl << endl;
+    cout << "median tracking time: " << vTimesTrack[nImages/2] << endl;
+    cout << "mean tracking time: " << totaltime/nImages << endl;
+
+    // Save camera trajectory
+    SLAM.SaveTrajectoryTUM("CameraTrajectory.txt");
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+#endif
+
+    return 0;
+}
+
+void LoadImages(const string &strAssociationFilename, vector<string> &vstrImageFilenamesRGB,
+                vector<string> &vstrImageFilenamesD, vector<double> &vTimestamps)
+{
+    ifstream fAssociation;
+    fAssociation.open(strAssociationFilename.c_str());
+    while(!fAssociation.eof())
+    {
+        string s;
+        getline(fAssociation,s);
+        if(!s.empty())
+        {
+            stringstream ss;
+            ss << s;
+            double t;
+            string sRGB, sD;
+            ss >> t;
+            vTimestamps.push_back(t);
+            ss >> sRGB;
+            vstrImageFilenamesRGB.push_back(sRGB);
+            ss >> t;
+            ss >> sD;
+            vstrImageFilenamesD.push_back(sD);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,33 @@
+# [작품 제목] 
+
+## 작품 개요
+1. 작품 설명1 or 목표 구현 내용
+2. 작품 설명2 
+3. 작품 설명3
+
+## System Architecture
+그림을 올리는 방법 
+[깃허브 README.md 파일에 이미지 업로드 하기](https://hanee24.github.io/2017/12/21/how-to-upload-image-with-github-readme/)를 참고해서 올려주세요.
+
+## Hardware Architecture
+
+위와 같이 올려주세요. :smile:  
+
+### Code Overview  
+- `소스코드1` : 이것을 의미합니다.
+- `소스코드2` : 이것도 의미하구요.
+- `소스코드3` : 저것도 의미합니다.
+
+### Project scenario
+
+1. 처음에 이거하고  
+2. 두번째 저거하고  
+3. 세번째 이거합니다. 여기 사진 넣어줘도 좋겠죠?  
+
+모두들 한학기동안 고생하셨습니다. 개인 github에 올리면 더 좋을 것 같아서 만들어봤습니다.  
+
+
+#### 참고자료
+- [[공통] 마크다운 markdown 작성법](https://gist.github.com/ihoneymon/652be052a0727ad59601) 
+- [[Jekyll Blog] 마크다운(Markdown) 사용법 및 예제](https://theorydb.github.io/envops/2019/05/22/envops-blog-how-to-use-md/)
+- 이모티콘을 첨부하고 싶다면 [여기](https://gist.github.com/rxaviers/7360908)를 참고해서 하시면 됩니다.
